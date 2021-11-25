d45d900a6a787e818cfe6337615d813c9e173ee2
laurencetuchin <laurencetuchin@gmail.com>
2021-11-25 18:32:11 +1100

    Major update

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  has_many :products, dependent: :destroy
+  has_one :cart
+  # Create a role for the user which each user can be both a buyer and a seller or both.
+  # enum role: [:buyer, :seller]
+  # after_initialize :set_default_role, :if => :new_record?
+
+  # def set_default_role
+  #   self.role ||= :buyer
+  # end
+
+
+  
 end
